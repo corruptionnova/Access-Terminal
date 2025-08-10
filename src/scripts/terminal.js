a74b299805f199bb06fc66b2c999894bcb2257b3
AveryMadness <averyfd@shaw.ca>
2025-08-10 13:47:38 -0600

    Update terminal.js

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fetchStringFromBackend(url, callback) {
         //temp
-        callback("90%");
+        callback("99%");
     }
 
     function setCookie(name, value, days = 365) {
